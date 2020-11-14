e35fdaaa2354c875e472839ce647f59f83ca4c54
Azer Balayev <34453759+Azer7@users.noreply.github.com>
2020-11-14 22:37:37 +0400

    chapter 2 complete

--- hello_world.py
+++ hello_world.py
@@ -4,3 +4,24 @@ print(message)
 message="Hello there 2"
 print(message)
 
+message="Hello there 3"
+print(message)
+
+
+name="john"
+surname="smith"
+full_name=f"{name.title()} he is {surname.title()}"
+full_name2="{} {}".format(name, surname)
+
+print(full_name2)
+print(f"\t{name} \n\t{surname}")
+
+resr=67_855
+
+print(resr)
+
+a = (1, 5, 50, 3, 9)
+x = min(a)
+print(x)
+
+import this
